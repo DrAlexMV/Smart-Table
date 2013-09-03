5c2f4a5e44b29ef51d1e63f4654ad69145284014
DrAlexMV <alexandermventura@utexas.edu>
2013-09-03 11:26:34 -0500

    Adding move to client script Adding tab html

---
+++ move_to_client.zsh
@@ -0,0 +1,4 @@
+echo Moving smart-table code to the client
+cp Smart-Table.debug.js ../../Client/app/bower_components/smart-table
+cp Smart-Table.min.js ../../Client/app/bower_components/smart-table
+echo Done
